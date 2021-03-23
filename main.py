c87f219876de920f8409e2062e9aa510f8d37da0
kgk1001 <kgk1001@outlook.com>
2021-03-23 10:48:31 +0530

    Update main.py
    started to code. tkinter lables

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-
+from tkinter import *
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print_hi('PyCharm')
-    print_hi('PyCharm')
+    root = Tk()
+    #creating a lable widget
+    title = Label(root,text = "Calculator")
+    creator = Label(root, text="Created by Nettie")
+    #this packs it into root
+    #title.pack()
+    #grid let you position lables
+    title.grid(row=0, column= 0)
+    creator.grid(row=0, column= 1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+    root.mainloop()
