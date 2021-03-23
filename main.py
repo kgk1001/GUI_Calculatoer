29a68a23f2be79b1407f532c3005331265f9af00
kgk1001 <kgk1001@outlook.com>
2021-03-23 21:35:42 +0530

    Update main.py
    grid

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from tkinter import *
+
 if __name__ == '__main__':
     root = Tk()
-    #creating a lable widget
-    title = Label(root,text = "Calculator")
+    # creating a lable widget
+    title = Label(root, text="Calculator")
     creator = Label(root, text="Created by Nettie")
-    #this packs it into root
-    #title.pack()
-    #grid let you position lables
-    title.grid(row=0, column= 0)
-    creator.grid(row=0, column= 1)
-
-
-
+    # this packs it into root
+    # title.pack()
+    # grid let you position lables
+    title.grid(row=0, column=0)
+    creator.grid(row=0, column=1)
     root.mainloop()
